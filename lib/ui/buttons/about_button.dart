3992e58f20fdbb28866bff4cd2f148e5e13d500d
GabrielHCRJ <gabrieldomingues@id.uff.br>
2023-11-10 15:45:34 -0300

    Site added in About popup without alignment

--- lib/ui/buttons/about_button.dart
+++ lib/ui/buttons/about_button.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutButton extends StatelessWidget {
   const AboutButton({super.key});
@@ -20,9 +23,37 @@ class AboutButton extends StatelessWidget {
           applicationVersion: "0.1.6",
           applicationIcon: SizedBox(
             height: 100,
-            child: const Image(image: AssetImage("assets/logo.png"))
+            child: Image.asset("assets/logo.png"),
           ),
+          applicationLegalese: "www.sampleforscience.org",
+          children: [
+            SizedBox(height: 16),
+        Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // Ajusta o alinhamento para a esquerda
+        children: [
+        Text(
+        'For more information, visit: ',
+        style: DefaultTextStyle.of(context).style,
+        ),
+
+        GestureDetector(
+        onTap: () {
+        launch('https://www.sampleforscience.org');
+        },
+        child: Text(
+        'Sample For Science',
+        style: TextStyle(
+        color: Colors.blue,
+        decoration: TextDecoration.underline,
+        ),
+        ),
+        ),
+        ],
+        ),
+
+          ],
         );
+        ;
       },
     );
   }
