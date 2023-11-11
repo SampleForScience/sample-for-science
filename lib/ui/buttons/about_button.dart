997341c97da8cdf8937a94f4d68b44044680fb9a
GabrielHCRJ <gabrieldomingues@id.uff.br>
2023-11-10 22:12:04 -0300

    About Popup Updated

--- lib/ui/buttons/about_button.dart
+++ lib/ui/buttons/about_button.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -23,32 +22,39 @@ class AboutButton extends StatelessWidget {
           applicationVersion: "0.1.6",
           applicationIcon: SizedBox(
             height: 100,
-            child: Image.asset("assets/logo.png"),
+            child: SvgPicture.asset("assets/logo.svg",
+              height: 100, // Defina a altura desejada
+              width: 100, // Defina a largura desejada
+            ),
           ),
-          applicationLegalese: "www.sampleforscience.org",
+          applicationLegalese: "© 2023 Sample. All rights reserved",
           children: [
-            SizedBox(height: 16),
-        Column(
-        crossAxisAlignment: CrossAxisAlignment.start, // Ajusta o alinhamento para a esquerda
-        children: [
-        Text(
-        'For more information, visit: ',
-        style: DefaultTextStyle.of(context).style,
-        ),
+            const SizedBox(height: 16),
+        SizedBox(
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center, // Ajusta o alinhamento para a esquerda
+          children: [
+          Text(
+          'For more information, visit: ',
+          style: DefaultTextStyle.of(context).style,
+          ),
 
-        GestureDetector(
-        onTap: () {
-        launch('https://www.sampleforscience.org');
-        },
-        child: Text(
-        'Sample For Science',
-        style: TextStyle(
-        color: Colors.blue,
-        decoration: TextDecoration.underline,
-        ),
-        ),
-        ),
-        ],
+          GestureDetector(
+          onTap: () {
+          launchUrl('https://www.sampleforscience.org' as Uri);
+          },
+          child: const Text(
+          'Sample For Science',
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 18,
+            decoration: TextDecoration.underline,
+
+                          ),
+                      ),
+                        ),
+                      ],
+          ),
         ),
 
           ],
