347b096838c35bf5c3f3f4f692726820a63e26d9
unknown <gabrieldomingues@id.uff.br>
2024-01-16 14:53:49 -0300

    Report bug button completed

--- lib/ui/buttons/report_bug_button.dart
+++ lib/ui/buttons/report_bug_button.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ReportBugButton extends StatefulWidget {
   const ReportBugButton({super.key});
@@ -12,6 +13,9 @@ class ReportBugButton extends StatefulWidget {
 
 class _ReportBugButtonState extends State<ReportBugButton> {
   late String packageVersion;
+  late TextEditingController _messageController;
+  late String _username;
+
   Future<void> getPackageVersion() async {
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
     setState(() {
@@ -21,7 +25,10 @@ class _ReportBugButtonState extends State<ReportBugButton> {
 
   @override
   void initState() {
+    Firebase.initializeApp();
     getPackageVersion();
+    _messageController = TextEditingController();
+    _username = FirebaseAuth.instance.currentUser?.displayName ?? 'Usuário Anônimo';
     super.initState();
   }
 
@@ -37,56 +44,70 @@ class _ReportBugButtonState extends State<ReportBugButton> {
       onTap: () {
         Navigator.pop(context);
         showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          actions: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                onPressed: () => {},
-                child: const Text('Send', style: TextStyle(fontSize: 16)),
+          context: context,
+          builder: (context) => AlertDialog(
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      // Envia a mensagem para o Firebase
+                      FirebaseFirestore.instance
+                          .collection('bugs')
+                          .add({
+                            'version': packageVersion,
+                            'username': _username,
+                            'message': _messageController.text,
+                          });
+
+                      // Mostra a mensagem de confirmação
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: Colors.green,
+                          content: Text( 'Thank you. Your report has been sent'),
+                        ),
+                      );
+
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Send', style: TextStyle(fontSize: 16)),
                   ),
-                TextButton(
-                onPressed: (){
-                Navigator.of(context).pop();
-              },
-              child: const Text('Close'),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Close'),
+                  ),
+                ],
               ),
-              ],
-            ),
-            
             ],
-            title: Center(child: const Text('Report a Bug')),           
-            
-            content: 
-                Container(
-                  height:150,
-                  width: 300,
-                  child: Column(                    
-                    
-                    children: [
-                      Text("Por favor, detalhe o bug que você achou"),
-                      Container(
-                        width: 280,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20)),
-                        
-                        child: TextField(
-                          decoration: InputDecoration(                            
-                            filled: false,
-                          ),
-                                             maxLines: 4,
-                                             ),
-                                              
+            title: Center(child: const Text('Report a Bug')),
+            content: Container(
+              height: 150,
+              width: 300,
+              child: Column(
+                children: [
+                  Text("Por favor, detalhe o bug que você achou"),
+                  Container(
+                    width: 280,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: TextField(
+                      controller: _messageController,
+                      decoration: InputDecoration(
+                        filled: false,
                       ),
-                    ],
+                      maxLines: 4,
+                    ),
                   ),
-                ),
-               
-            
-        ));
+                ],
+              ),
+            ),
+          ),
+        );
       },
     );
   }
