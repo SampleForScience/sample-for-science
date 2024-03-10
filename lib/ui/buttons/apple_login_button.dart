c8d6661d8f87238229d1cbd1e165f3b02eb9c494
marcos <marcosfernando@id.uff.br>
2024-03-10 12:39:45 -0300

    popup added

--- lib/ui/buttons/apple_login_button.dart
+++ lib/ui/buttons/apple_login_button.dart
@@ -23,7 +23,27 @@ class _LoginButtonState extends State<AppleLoginButton> {
     return SignInButton(
       Buttons.appleDark,
       text: "Login with Apple",
-      onPressed: _signInHandler.signInWithApple,
+      onPressed: () async {
+        await showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("Email Sharing"),
+              content: const Text("For your account to be successfully created, we need you to agree to share your email on the first login"),
+              actions: [
+                TextButton(
+                  child: const Text("OK"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+
+        _signInHandler.signInWithApple();
+      },
     );
   }
 }
