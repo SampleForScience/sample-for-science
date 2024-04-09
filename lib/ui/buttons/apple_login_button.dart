1a1515021b274b2d8c3c333c107991dda19768a5
unknown <gabrieldomingues@id.uff.br>
2024-04-08 22:48:51 -0300

    New Login Page

--- lib/ui/buttons/apple_login_button.dart
+++ lib/ui/buttons/apple_login_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sample/services/signInHandler.dart';
 import 'package:sign_in_button/sign_in_button.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AppleLoginButton extends StatefulWidget {
   const AppleLoginButton({super.key});
@@ -20,10 +21,47 @@ class _LoginButtonState extends State<AppleLoginButton> {
 
   @override
   Widget build(BuildContext context) {
-    return SignInButton(
-      Buttons.appleDark,
-      text: "Login with Apple",
-      onPressed: () async {
+    return GestureDetector(
+      child: Container(
+        width: 325,
+        height: 48,
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color.fromRGBO(43, 52, 70, 1),
+                Color.fromRGBO(88, 131, 158, 1),
+              ],
+            ),
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                offset: Offset(-2, 2),
+                blurRadius: 4,
+                spreadRadius: 2,
+              )
+            ]),
+        child: const Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FaIcon(FontAwesomeIcons.apple),
+              SizedBox(width: 24),
+              Text(
+                "Login with Apple",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Color.fromRGBO(202, 207, 203, 1),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      onTap: () async {
         // await showDialog(
         //   context: context,
         //   builder: (BuildContext context) {
--- lib/ui/buttons/google_login_button.dart
+++ lib/ui/buttons/google_login_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sample/services/signInHandler.dart';
-import 'package:sign_in_button/sign_in_button.dart';
 
 class GoogleLoginButton extends StatefulWidget {
   const GoogleLoginButton({super.key});
@@ -21,10 +20,47 @@ class _LoginButtonState extends State<GoogleLoginButton> {
 
   @override
   Widget build(BuildContext context) {
-    return SignInButton(
-      Buttons.googleDark,
-      text: "Login with Google",
-      onPressed: _signInHandler.signInWithGoogle,
+    return GestureDetector(
+      child: Container(
+        width: 325,
+        height: 48,
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color.fromRGBO(43, 52, 70, 1),
+                Color.fromRGBO(88, 131, 158, 1),
+              ],
+            ),
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                offset: Offset(-2, 2),
+                blurRadius: 4,
+                spreadRadius: 2,
+              )
+            ]),
+        child: const Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FaIcon(FontAwesomeIcons.google),
+              SizedBox(width: 24),
+              Text(
+                "Login with Google",
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Color.fromRGBO(202, 207, 203, 1),
+                    fontFamily: 'AvenirRoman'),
+              ),
+            ],
+          ),
+        ),
+      ),
+      onTap: _signInHandler.signInWithGoogle,
     );
   }
 }
