162c88d968e98edf0ddb5e58c3407a3bff9085f0
unknown <gabrieldomingues@id.uff.br>
2024-05-05 10:58:48 -0300

    New Layout / issues with Logout Button

--- lib/ui/views/terms_of_use.dart
+++ lib/ui/views/terms_of_use.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sample/services/signInHandler.dart';
 
 class TermsOfUsePage extends StatefulWidget {
   const TermsOfUsePage({Key? key}) : super(key: key);
@@ -14,7 +15,6 @@ class _TermsOfUsePageState extends State<TermsOfUsePage> {
   bool _termsAccepted = false;
 
   Future<void> _acceptTerms() async {
-    // Salvando a aceitação dos termos no Firebase
     final currentUser = FirebaseAuth.instance.currentUser;
     if (currentUser != null) {
       try {
@@ -26,11 +26,10 @@ class _TermsOfUsePageState extends State<TermsOfUsePage> {
           _termsAccepted = true;
         });
         print('Termos aceitos com sucesso.');
-        // Redirecionar o usuário para a próxima página (por exemplo, a página de pesquisa)
+
         Navigator.pushReplacementNamed(context, '/search');
       } catch (e) {
         print('Erro ao aceitar os termos: $e');
-        // Tratar o erro, se necessário
       }
     } else {
       print('Usuário atual não encontrado.');
@@ -40,29 +39,61 @@ class _TermsOfUsePageState extends State<TermsOfUsePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Terms of Use'),
-      ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Terms of Use',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        padding: const EdgeInsets.all(0),
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                Color.fromARGB(255, 255, 255, 255),
+                Color.fromARGB(255, 153, 153, 236)
+              ])),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 48),
+                Text(
+                  'Terms of Use',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'This software is not intended for commercial purposes. The use of this software for any commercial purpose, including, but not limited to:\n',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  '- Sale of products or services\n- Advertising or marketing\n- Collection of customer data\n- Use in a business environment\n',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                    'Is strictly prohibited.\nBy using this software, you agree to:\n'),
+                Text(
+                    '- Not use it for any commercial purpose\n- Not distribute it to third parties without the prior written consent of the developer\n- Not remove or modify any copyright notices or trademarks of the software\n',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Text(
+                    'The developer is not liable for any damages or losses incurred as a result of using this software for commercial purposes.\n'),
+                Text(
+                    'If you are interested in using this software for commercial purposes, please contact the developer to discuss licensing options.\n'),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: _termsAccepted ? null : _acceptTerms,
+                      child:
+                          Text(_termsAccepted ? 'Terms Accepted' : 'I Agree'),
+                    ),
+                    ElevatedButton(onPressed: null, child: const Text('Logout'))
+                  ],
+                ),
+              ],
             ),
-            SizedBox(height: 16),
-            Text(
-              'EU AUTORIZO USAREM MEUS DADOS PRO QUE QUISEREM\n PROMETO TAMBÉM NÃO PROCESSAR NINGUÉM\n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapibus libero ac velit tincidunt, et pharetra elit sollicitudin. Nam auctor dolor id eleifend egestas. Nam congue mauris nec lorem blandit, nec dapibus metus tempor. Donec aliquet posuere est sit amet tempus. Nam porta vehicula enim, id posuere lorem convallis vel. Integer dictum tortor at purus mattis, nec interdum neque lobortis. Vivamus in feugiat nunc. In sagittis malesuada orci sit amet venenatis. Duis euismod est quis malesuada feugiat. Proin tempus dolor quis leo viverra, in malesuada justo tempus. Maecenas auctor, ipsum nec pharetra rhoncus, sapien dui laoreet orci, eget lacinia nisl sapien in risus. Integer quis purus at dolor euismod sollicitudin. Phasellus non scelerisque lacus, nec ullamcorper orci. Proin semper sodales enim id interdum.',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _termsAccepted ? null : _acceptTerms,
-              child: Text(_termsAccepted ? 'Terms Accepted' : 'I Agree'),
-            ),
-          ],
+          ),
         ),
       ),
     );
