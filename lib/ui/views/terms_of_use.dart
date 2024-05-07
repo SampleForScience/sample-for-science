bf110a1d6bfb66968e263e4c8337ac6f716a77eb
MarcosFernando <marcosfernando@id.uff.br>
2024-05-07 12:39:12 -0300

    terms of service page bugfix

--- lib/ui/views/terms_of_use.dart
+++ lib/ui/views/terms_of_use.dart
@@ -59,49 +59,51 @@ class _TermsOfUsePageState extends State<TermsOfUsePage> {
               ])),
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 48),
-                Text(
-                  'Terms of Use',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 16),
-                Text(
-                  'This software is not intended for commercial purposes. The use of this software for any commercial purpose, including, but not limited to:\n',
-                  style: TextStyle(fontSize: 16),
-                ),
-                Text(
-                  '- Sale of products or services\n- Advertising or marketing\n- Collection of customer data\n- Use in a business environment\n',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                    'Is strictly prohibited.\nBy using this software, you agree to:\n'),
-                Text(
-                    '- Not use it for any commercial purpose\n- Not distribute it to third parties without the prior written consent of the developer\n- Not remove or modify any copyright notices or trademarks of the software\n',
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                Text(
-                    'The developer is not liable for any damages or losses incurred as a result of using this software for commercial purposes.\n'),
-                Text(
-                    'If you are interested in using this software for commercial purposes, please contact the developer to discuss licensing options.\n'),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: _termsAccepted ? null : _acceptTerms,
-                      child:
-                          Text(_termsAccepted ? 'Terms Accepted' : 'I Agree'),
-                    ),
-                    ElevatedButton(
-                        onPressed: _signInHandler.signInWithGoogle,
-                        child: const Text('Logout')
-                    )
-                  ],
-                ),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 48),
+                  Text(
+                    'Terms of Use',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    'This software is not intended for commercial purposes. The use of this software for any commercial purpose, including, but not limited to:\n',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    '- Sale of products or services\n- Advertising or marketing\n- Collection of customer data\n- Use in a business environment\n',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                      'Is strictly prohibited.\nBy using this software, you agree to:\n'),
+                  Text(
+                      '- Not use it for any commercial purpose\n- Not distribute it to third parties without the prior written consent of the developer\n- Not remove or modify any copyright notices or trademarks of the software\n',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Text(
+                      'The developer is not liable for any damages or losses incurred as a result of using this software for commercial purposes.\n'),
+                  Text(
+                      'If you are interested in using this software for commercial purposes, please contact the developer to discuss licensing options.\n'),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: _termsAccepted ? null : _acceptTerms,
+                        child:
+                            Text(_termsAccepted ? 'Terms Accepted' : 'I Agree'),
+                      ),
+                      ElevatedButton(
+                          onPressed: _signInHandler.signInWithGoogle,
+                          child: const Text('Logout')
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
